AdeWang0629/Python_Mtgjson

commit 8126fb0f246ffdf8be704446796fb413c172ff35

add Scryfall as a first source for TCGPlayer Product IDs (#234)
* added Scryfall as a first source for TCGPlayer Product ID

* reformatted with black

* reverted to original doc for add_tcgplayer_fields

* fix spacing

Signed-off-by: Zach Halpern <ZaHalpern+github@gmail.com>

mtgjson4/provider/tcgplayer.py
@@ -158,7 +158,9 @@ def get_card_property(
     :return: Value of field
     """
     for card in card_list:
-        if card_name.lower() == card["name"].lower():
+        # will try to match against Part A for split cards (if applicable)
+        part_a_name = card_name.split("//")[0].strip()
+        if card_name.lower() == part_a_name.lower():
             return card.get(card_field, None)
 
     LOGGER.warning("Unable to find card {} in TCGPlayer card list".format(card_name))

mtgjson4/compile_mtg.py
@@ -212,15 +212,17 @@ def add_tcgplayer_fields(
     tcg_card_objs = tcgplayer.get_group_id_cards(group_id)
 
     for card in cards:
-        prod_id = tcgplayer.get_card_property(card["name"], tcg_card_objs, "productId")
+        # No need to fetch from TCGPlayer if already found in Scryfall
+        if not card["tcgplayerProductId"]:
+            card["tcgplayerProductId"] = tcgplayer.get_card_property(
+                card["name"], tcg_card_objs, "productId"
+            )
         prod_url = tcgplayer.get_card_property(card["name"], tcg_card_objs, "url")
 
-        if prod_id and prod_url:
-            card["tcgplayerProductId"] = prod_id
+        if card["tcgplayerProductId"] and prod_url:
             card["tcgplayerPurchaseUrl"] = tcgplayer.log_redirection_url(
                 card["tcgplayerProductId"], prod_url
             )
-
     return cards
 
 
@@ -635,6 +637,7 @@ def build_mtgjson_card(
     mtgjson_card["number"] = sf_card.get("collector_number")
     mtgjson_card["isReserved"] = sf_card.get("reserved")
     mtgjson_card["frameEffect"] = sf_card.get("frame_effect")
+    mtgjson_card["tcgplayerProductId"] = sf_card.get("tcgplayer_id")
 
     # Vanguard fields
     mtgjson_card["life"] = sf_card.get("life_modifier")

commit 44f5fa3b8f0ec4d921d32a518d0d29799b53602b

Fix issue introduced by #234
Signed-off-by: Zach Halpern <ZaHalpern+github@gmail.com>

mtgjson4/provider/tcgplayer.py
@@ -158,9 +158,10 @@ def get_card_property(
     :return: Value of field
     """
     for card in card_list:
-        # will try to match against Part A for split cards (if applicable)
-        part_a_name = card_name.split("//")[0].strip()
-        if card_name.lower() == part_a_name.lower():
+        input_fix_split = card_name.split("//")[0].strip()
+        list_fix_split = card["name"].split("//")[0].strip()
+
+        if list_fix_split.lower() == input_fix_split.lower():
             return card.get(card_field, None)
 
     LOGGER.warning("Unable to find card {} in TCGPlayer card list".format(card_name))